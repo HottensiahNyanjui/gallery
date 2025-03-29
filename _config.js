5eab992549282b32c1d2391a328cc3f16afe395e
HottensiahNyanjui <hottensiahnyanjui@gmail.com>
2025-03-29 18:42:26 +0300

    Corrected Database

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://hottensiahnyanjui:HottieShiko2014!@ip1.lodkxp6.mongodb.net/?retryWrites=true&w=majority&appName=IP1',
-    development: 'mongodb+srv://hottensiahnyanjui:HottieShiko2014!@ip1.lodkxp6.mongodb.net/?retryWrites=true&w=majority&appName=IP1',
-    test: 'mongodb+srv://hottensiahnyanjui:HottieShiko2014!@ip1.lodkxp6.mongodb.net/?retryWrites=true&w=majority&appName=IP1',
+    production: 'mongodb+srv://hottensiahnyanjui:HottieShiko2014!@ip1.lodkxp6.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=IP1',
+    development: 'mongodb+srv://hottensiahnyanjui:HottieShiko2014!@ip1.lodkxp6.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=IP1',
+    test: 'mongodb+srv://hottensiahnyanjui:HottieShiko2014!@ip1.lodkxp6.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=IP1',
 }
 module.exports = config;

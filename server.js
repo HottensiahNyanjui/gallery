470b45c82997c8df7fd7949c7b1f00b18ccf60dc
HottensiahNyanjui <hottensiahnyanjui@gmail.com>
2025-03-31 22:12:27 +0300

    server.js conflict resolve

--- server.js
+++ server.js
@@ -10,36 +10,15 @@ const config = require('./_config');
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-<<<<<<< HEAD
 const mongodb_url = process.env.MONGODB_URI || 'mongodb://localhost:27017/darkroom';
 
 // connecting the database
 mongoose.connect(mongodb_url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('✅ Connected to MongoDB successfully'))
     .catch((err) => console.error('MongoDB connection error:', err));
-=======
+
 // Initializing the app
 const app = express();
-
-// connecting the database
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
-});
->>>>>>> test
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
-
 
 
 // View Engine
